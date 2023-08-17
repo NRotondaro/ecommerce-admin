a71d9f19ad68b904545b21670809dbb381cf4121
NRotondaro <niksonrotondaro1@gmail.com>
2023-08-17 00:10:58 -0300

    feat: finish order entity

--- app/(dashboard)/[storeId]/(routes)/orders/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -2,8 +2,9 @@ import { format } from 'date-fns';
 
 import prismadb from '@/lib/prismadb';
 
-import { BillboardClient } from './components/client';
-import { BillboardColumn } from './components/columns';
+import { OrderClient } from './components/client';
+import { OrderColumn } from './components/columns';
+import { formatter } from '@/lib/utils';
 
 const OrdersPage = async ({ params }: { params: { storeId: string } }) => {
   const orders = await prismadb.order.findMany({
@@ -12,7 +13,9 @@ const OrdersPage = async ({ params }: { params: { storeId: string } }) => {
     },
     include: {
       orderItems: {
-        product: true,
+        include: {
+          product: true,
+        },
       },
     },
     orderBy: {
@@ -20,18 +23,26 @@ const OrdersPage = async ({ params }: { params: { storeId: string } }) => {
     },
   });
 
-  const formatedOrders: BillboardColumn[] = orders.map((item) => ({
+  const formatedOrders: OrderColumn[] = orders.map((item) => ({
     id: item.id,
     phone: item.phone,
     address: item.address,
-    products: item.storeId
+    products: item.orderItems
+      .map((orderItem) => orderItem.product.name)
+      .join(', '),
+    totalPrice: formatter.format(
+      item.orderItems.reduce((total, item) => {
+        return total + Number(item.product.price);
+      }, 0)
+    ),
+    isPaid: item.isPaid,
     createdAt: format(item.createdAt, 'MMMM do, yyyy'),
   }));
 
   return (
     <div className='flex-col'>
       <div className='flex-1 space-y-4 p-8 pt-6'>
-        <BillboardClient data={formatedOrders} />
+        <OrderClient data={formatedOrders} />
       </div>
     </div>
   );
